31d8df41e48ff9705bcaa45f55a1c55bb1ea061f
bkmi <12955549+bkmi@users.noreply.github.com>
2023-02-06 11:36:41 +0100

    added res nets

--- zuko/nn.py
+++ zuko/nn.py
@@ -7,6 +7,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from torch import Tensor, BoolTensor
+from torch.nn import init
 from typing import *
 
 
@@ -106,6 +107,100 @@ class MLP(nn.Sequential):
 
         self.in_features = in_features
         self.out_features = out_features
+
+
+class ResidualBlock(nn.Module):
+    """A general-purpose residual block. Works only with 1-dim inputs."""
+
+    def __init__(
+        self,
+        features,
+        context_features,
+        activation=F.relu,
+        dropout_probability=0.0,
+        use_layer_norm=False,
+        zero_initialization=True,
+    ):
+        super().__init__()
+        self.activation = activation
+
+        self.use_layer_norm = use_layer_norm
+        if use_layer_norm:
+            self.layer_norm_layers = nn.ModuleList(
+                [nn.LayerNorm(features, eps=1e-6) for _ in range(2)]
+            )
+        if context_features is not None:
+            self.context_layer = nn.Linear(context_features, features)
+        self.linear_layers = nn.ModuleList(
+            [nn.Linear(features, features) for _ in range(2)]
+        )
+        self.dropout = nn.Dropout(p=dropout_probability)
+        if zero_initialization:
+            init.uniform_(self.linear_layers[-1].weight, -1e-3, 1e-3)
+            init.uniform_(self.linear_layers[-1].bias, -1e-3, 1e-3)
+
+    def forward(self, inputs, context=None):
+        temps = inputs
+        if self.use_layer_norm:
+            temps = self.layer_norm_layers[0](temps)
+        temps = self.activation(temps)
+        temps = self.linear_layers[0](temps)
+        if self.use_layer_norm:
+            temps = self.layer_norm_layers[1](temps)
+        temps = self.activation(temps)
+        temps = self.dropout(temps)
+        temps = self.linear_layers[1](temps)
+        if context is not None:
+            temps = F.glu(torch.cat((temps, self.context_layer(context)), dim=1), dim=1)
+        return inputs + temps
+
+
+class ResidualNet(nn.Module):
+    """A general-purpose residual network. Works only with 1-dim inputs."""
+
+    def __init__(
+        self,
+        in_features,
+        out_features,
+        hidden_features,
+        context_features=None,
+        num_blocks=2,
+        activation=F.relu,
+        dropout_probability=0.0,
+        use_layer_norm=False,
+    ):
+        super().__init__()
+        self.hidden_features = hidden_features
+        self.context_features = context_features
+        if context_features is not None:
+            self.initial_layer = nn.Linear(
+                in_features + context_features, hidden_features
+            )
+        else:
+            self.initial_layer = nn.Linear(in_features, hidden_features)
+        self.blocks = nn.ModuleList(
+            [
+                ResidualBlock(
+                    features=hidden_features,
+                    context_features=context_features,
+                    activation=activation,
+                    dropout_probability=dropout_probability,
+                    use_layer_norm=use_layer_norm,
+                )
+                for _ in range(num_blocks)
+            ]
+        )
+        self.final_layer = nn.Linear(hidden_features, out_features)
+
+    def forward(self, inputs, context=None):
+        if context is None:
+            temps = self.initial_layer(inputs)
+        else:
+            temps = self.initial_layer(torch.cat((inputs, context), dim=1))
+        for block in self.blocks:
+            temps = block(temps, context=context)
+        outputs = self.final_layer(temps)
+        return outputs
 
 
 class MaskedLinear(nn.Linear):
